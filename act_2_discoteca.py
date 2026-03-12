97a832259f44348581850f23688d32c643575496
caanvale <carlosvaramirez@gmail.com>
2026-03-11 20:10:52 -0500

    act 2
    act 2

---
+++ act_2_discoteca.py
@@ -0,0 +1,43 @@
+resultado = (5 + 3) * 2
+
+print(resultado)
+
+i = 0
+while i < 5:
+
+    print (i)
+
+    i += 1
+
+    #asignar las variables según el tipo de dato
+    a = float (3)
+    b = int (3)
+    c = str (3)
+
+    #IMprimir en pantalla el resultado acorde a las variables asignadas
+    print (f'el 3 en dato flotante es: {a}')
+    print (f'el 3 en dato enetro es: {b}')
+    print (f'el 3 en dato cadena es: {c}')
+
+    #sumar c
+    d = b+b
+
+
+    #imprimir suma en pantalla
+    print(f'la suma es: {d}')
+
+    ## second
+    # asignar de varibale según el tipo de dato
+
+    a = [1, 2, 3, 4, 5]
+    b = (1, 2, 3)
+    c = {1, 2, 3, 4}
+    d = {'nombre' : 'juan', 'edad' : 25}
+
+    # imprimir en pantalla el resultado acorde a las variables asignadas
+    print(f'una lista es: {a}')
+    print(f'una tupla es: {b}')
+    print(f'un conjunto es: {c}')
+    print(f'un diccionario es: {d}')
+
+
